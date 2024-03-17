05a1041e0314fd523c8b5456df3ac74cc667189a
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-03-17 20:30:38 +0330

    Added C# Class and AI model for image recognition.

---
+++ ComputerTNB_ClassMgr_Bot/AI_ImgProc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenCvSharp;
+using OpenCvSharp.Face;
+
+namespace ComputerTNB_ClassMgr_Bot
+{
+    /// <summary>
+    /// Provides functionalities used for AI Image Processing.
+    /// </summary>
+    public class AI_ImgProc
+    {
+
+    }
+}
